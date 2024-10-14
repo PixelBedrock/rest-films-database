1f5cd5c52c82bc2ec7e57772907ea60fe06d056d
PixelBedrock <113065795+PixelBedrock@users.noreply.github.com>
2024-10-14 12:13:16 +0100

    chore: disable eslint rule for express next() type

--- src/logging.ts
+++ src/logging.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export default function(req: Request, _: Response, next: Function) {
     console.log(`${req.ip} - [${new Date().toLocaleTimeString()}] ${req.method} ${req.originalUrl} "${req.header("User-Agent")}"`);
     next();
